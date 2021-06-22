e26eaaf151c5eceeb0e14bced39bbb296bf5919c
a-Marino <andresmarinolaiolo@hotmail.com.ar>
2021-06-22 20:08:46 -0300

    router funcionando

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
+import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { library } from '@fortawesome/fontawesome-svg-core'
@@ -8,42 +9,11 @@ import { faBars } from '@fortawesome/free-solid-svg-icons'
 import {faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import '@/assets/css/tailwind.css'
-//importo la libreria vuerouter
-import VueRouter from 'vue-router';
-
-//importamos nuestros componentes
-import About from './components/About';
-import Canciones from './components/Canciones';
-import Contacto from './components/Contacto';
-
 
 
 library.add(faBars, faShoppingCart)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
-//crear componente
-Vue.component('About',About);
-Vue.component('Canciones',Canciones);
-Vue.component('Contacto',Contacto);
-
-
-
-//uso de vue-router
-Vue.use(VueRouter);
-
-//defino un arreglo con las rutas
-const routes = [
-  {path: '/', component:Canciones, name:'canciones'},
-  {path: '/about', component:About, name:'about'},
-  {path: '/contacto', component:About, name:'contacto'}
-
-]
-
-//creo el objeto router
-const router = new VueRouter({
-  routes,
-  mode:'history' //elimina el simbolo # en las url
-})
 
 
 Vue.config.productionTip = false
