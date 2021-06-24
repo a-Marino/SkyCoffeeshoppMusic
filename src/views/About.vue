af2446c9be5ffd9ac1c0a8912a4f5b654b3778d2
Ignacio Coria <ignaciocoriadb@gmail.com>
2021-06-23 21:30:33 -0300

    about img

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,12 +1,50 @@
 <template>
+   <div>
     <header>
+
+        <section class="section-1 d-flex justify-content-center align-items-center">
+       
+        <h1 class="text-5xl">ABOUT</h1> 
+
+        <img src="https://s3.amazonaws.com/beatstarsdata/proweb/2.0/user-styles/uploads/published/2876769/main_bg_mobile.jpg?_=1622162090" alt="" class="img-circle">
         
+        <h2 class="text-2xl">Biography</h2>
+        <p class="text-1xl">Argentine Beatmaker, Rapper and visual artist.</p>
+       
+        </section>
     </header>
+   </div>
 </template>
 
 
 <script>
+
 export default {
-    name: 'about'
+    name:'about'
+    
 }
 </script>
+
+
+<style>
+
+img {
+  border-radius: 50%;
+  width:200px;
+  display:block;
+  margin: 50px;
+}
+.section-1 {
+    min-height: 50vh;
+    background-position: center;
+}
+
+h1{
+    color: #ffff;
+     display:block;
+  margin: 50px;
+}
+
+   
+    
+</style>
